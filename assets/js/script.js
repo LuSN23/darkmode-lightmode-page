582e149974f3d2211ed64b288586139a7df445da
Luciana Sarai Nunes <lucianasarainunes@gmail.com>
2022-10-07 10:34:22 -0300

    selecionando os elementos html

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+//Selecionando os elementos html e armazenando-os em variáveis
+const h1 = document.getElementById('page-title');
+const button = document.getElementById('mode-selector');
+const footer = document.getElementsByTagName('footer')[0]; //Já que aqui retorna um HTMLCollection é preciso selecionar 1 item.
+const body = document.getElementsByTagName('body')[0];
+
+
+
