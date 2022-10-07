822d3a719d70d4f539019a1f55ee34874e614835
Luciana Sarai Nunes <lucianasarainunes@gmail.com>
2022-10-07 10:36:55 -0300

    colocando um listener no elemento button

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,5 +4,6 @@ const button = document.getElementById('mode-selector');
 const footer = document.getElementsByTagName('footer')[0]; //Já que aqui retorna um HTMLCollection é preciso selecionar 1 item.
 const body = document.getElementsByTagName('body')[0];
 
-
+//Colocando um listener de eventos no elemento button
+button.addEventListener('click', changeMode);
 
